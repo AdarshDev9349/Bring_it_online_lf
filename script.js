18f3e753518707a4c50d5ef92cabec0504eb0b3c
AdarshDev9349 <adarshdev9349@gmail.com>
2023-11-18 14:18:27 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,8 @@
+
+  var typed = new Typed('#typed', {
+    strings: ["What's on our menu...?","What's on our menu...?"],
+    typeSpeed: 30,
+    backSpeed: 50,
+    loop: true,
+    showCursor: false
+});
